19eb8f1fc8b11c4fa0b012b95f04bca7a9ede4c0
Dustin Myers <dustint314@gmail.com>
2021-01-21 11:57:30 -0700

    wip

--- followAlong/src/components/Siblings.js
+++ followAlong/src/components/Siblings.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { FamilyContext } from "../App";
 
 const Siblings = (props) => {
   return (
     <section className="parents">
-      {props.family.siblings.map((p) => (
-        <div className="person" key={p.name}>
-          <img width="150" src={p.img} alt={p.name} />
-          <strong>{p.name}</strong>
-        </div>
-      ))}
+      <FamilyContext.Consumer>
+        {(values) => (
+          <>
+            {values.activeFamily.siblings.map((p) => (
+              <div className="person" key={p.name}>
+                <img width="150" src={p.img} alt={p.name} />
+                <strong>{p.name}</strong>
+              </div>
+            ))}
+          </>
+        )}
+      </FamilyContext.Consumer>
     </section>
   );
 };
